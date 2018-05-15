00e3f789709cada38bc1fbef1859d2ca53afca8b
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2018-05-15 23:15:21 +0100

    Testing get data from mongo

--- handlers/shop/shop.go
+++ handlers/shop/shop.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/jalopezma/shops-api/common"
 	"github.com/jalopezma/shops-api/schemas"
 )
 
@@ -25,7 +26,10 @@ func Shops(w http.ResponseWriter, r *http.Request) {
 }
 
 // GetShop -
-func Shop(w http.ResponseWriter, r *http.Request) {}
+func Shop(w http.ResponseWriter, r *http.Request) {
+	shop := common.Deps.Shops.Get("something")
+	log.Printf("GetShop: %+v", shop)
+}
 
 // CreateShop -
 func CreateShop(w http.ResponseWriter, r *http.Request) {}
