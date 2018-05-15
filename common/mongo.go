8e75e1c879c40178ef9305c7815474f8dce5b10d
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2018-05-15 23:11:55 +0100

    ParseUrl; Save mgo session, dialInfo and database

--- common/mongo.go
+++ common/mongo.go
@@ -6,18 +6,41 @@ import (
 	"gopkg.in/mgo.v2"
 )
 
-var mgoSession *mgo.Session
+type mgoData struct {
+	session  *mgo.Session
+	dialInfo *mgo.DialInfo
+	db       *mgo.Database
+}
+
+var mgoSession mgoData
 
 // GetMongoSession Creates a new session if mgoSession is nil i.e there is no active mongo session.
 // If there is an active mongo session it will return a Clone
-func GetMongoSession() *mgo.Session {
-	if mgoSession == nil {
-		var err error
-		// to improve this
-		mgoSession, err = mgo.Dial(mongo_conn_str)
-		if err != nil {
-			log.Fatal("Failed to start the Mongo session")
-		}
+func GetMongoSession() (session *mgo.Session) {
+	if mgoSession.session != nil {
+		session = mgoSession.session.Clone()
 	}
-	return mgoSession.Clone()
+	return
+}
+
+// ConnectMongo - Connects mongo and returns a clone of the session
+func ConnectMongo(url string, db string) *mgo.Session {
+	var err error
+	// to improve this
+	log.Printf("[ConnectMongo] url: %q", url)
+
+	mgoSession = mgoData{}
+	mgoSession.dialInfo, err = mgo.ParseURL(url)
+	if err != nil {
+		log.Printf("[ConnectMongo] Couldn't parse mongodb url %q\n%v", url, err)
+	}
+
+	mgoSession.session, err = mgo.Dial(url)
+	if err != nil {
+		log.Fatal("[ConnectMongo] Failed to start the Mongo session")
+	}
+
+	mgoSession.db = mgoSession.session.DB(db)
+	//log.Printf("\n%+v\n%+v", mgoSession.dialInfo, mgoSession.db)
+	return mgoSession.session.Clone()
 }
