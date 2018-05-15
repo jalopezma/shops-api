da04494995025cde9cb77c9f9e2ca9e9bcbeccd0
Jose Lopez <joseadrian.lopezmartin@gmail.com>
2018-05-15 23:13:40 +0100

    Replace shop by shops

--- handlers/router.go
+++ handlers/router.go
@@ -10,10 +10,10 @@ func NewRouter() *mux.Router {
 
 	router := mux.NewRouter()
 	v1 := router.PathPrefix("/v1").Subrouter()
-	v1.HandleFunc("/shop", shop.Shops).Methods("GET")
-	v1.HandleFunc("/shop/{id:[a-zA-Z-]{5,10}}", shop.Shop).Methods("GET")
-	v1.HandleFunc("/shop/{id:[a-zA-Z-]}", shop.CreateShop).Methods("POST")
-	v1.HandleFunc("/shop/{id:[a-zA-Z-]}", shop.DeleteShop).Methods("DELETE")
+	v1.HandleFunc("/shops", shop.Shops).Methods("GET")
+	v1.HandleFunc("/shops/{id:[a-zA-Z-]{5,10}}", shop.Shop).Methods("GET")
+	v1.HandleFunc("/shops/{id:[a-zA-Z-]}", shop.CreateShop).Methods("POST")
+	v1.HandleFunc("/shops/{id:[a-zA-Z-]}", shop.DeleteShop).Methods("DELETE")
 
 	return router
 }
